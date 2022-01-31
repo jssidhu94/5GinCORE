f136801abfb772540aea58263c7feb0f449bce03
Jagdeep Singh <js.sidhu94@gmail.com>
2022-01-30 23:54:17 -0800

    Topology

---
+++ script.sh
@@ -0,0 +1,18 @@
+docker pull rdefosseoai/oai-amf:develop
+docker pull rdefosseoai/oai-nrf:develop
+docker pull rdefosseoai/oai-spgwu-tiny:develop
+docker pull rdefosseoai/oai-smf:develop
+docker pull rdefosseoai/oai-udr:develop
+docker pull rdefosseoai/oai-udm:develop
+docker pull rdefosseoai/oai-ausf:develop
+docker pull rdefosseoai/oai-upf-vpp:develop
+
+docker image tag rdefosseoai/oai-amf:develop oai-amf:latest
+docker image tag rdefosseoai/oai-nrf:develop oai-nrf:latest
+docker image tag rdefosseoai/oai-smf:develop oai-smf:latest
+docker image tag rdefosseoai/oai-spgwu-tiny:develop oai-spgwu-tiny:latest
+docker image tag rdefosseoai/oai-udr:develop oai-udr:latest
+docker image tag rdefosseoai/oai-udm:develop oai-udm:latest
+docker image tag rdefosseoai/oai-ausf:develop oai-ausf:latest
+docker image tag rdefosseoai/oai-upf-vpp:develop oai-upf-vpp:latest
+
