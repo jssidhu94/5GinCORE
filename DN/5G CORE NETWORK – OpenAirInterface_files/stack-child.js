460a5b342a612a9939252a448e3d0b97c2a89d90
core <core@coreemu5.2>
2022-02-26 16:48:16 +0100

    code refraction

---
+++ DN/5G CORE NETWORK – OpenAirInterface_files/stack-child.js
@@ -0,0 +1,7 @@
+
+/* CUSTOM OAI */
+/* Make Dropdown Work on Hover */
+
+jQuery(document).ready(function ($) {
+    $('#strategic-members-slider img, #nonprofit-members-slider img, #associate-members-slider img').removeAttr('width').removeAttr('height');
+    });
